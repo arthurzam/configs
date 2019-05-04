9274572861fa4c89af2f055900dd70ff0a9eaa56
Zamarin Arthur <arthurzam@gmail.com>
2019-05-04 15:15:48 +0300

    bash & vim & xinit cleanup

--- installer.sh
+++ installer.sh
@@ -4,16 +4,21 @@ _symlink() {
     # $1 - src
     # $2 - dst
     
+    local src="$(realpath "$1")"
+
     [[ $# != 2 ]] && echo "bad amount [$#] of arguments to symlink" && return 1
+    [[ "$(realpath "$2")" = "${src}" ]] && echo "already linked" && return 0
 
     [[ -d "$2" ]] && rm -r "$2" && echo "Removed directory $2"
     [[ -e "$2" ]] && rm "$2" && echo "Removed file $2"
-    [[ ! -e "$2" ]] && ln -s "$(realpath "$1")" "$2" && echo "Linked $2 <- $1"
+    [[ ! -e "$2" ]] && ln -s "${src}" "$2" && echo "Linked $2 <- $1"
 }
 
 _parse_install_cfg() {
     cat "INSTALL.cfg" | while read line; do
         echo ">>> ${line}"
+
+        line="$(sed -e 's:~:'"${HOME}:g" <<< "${line}")"
         
         local  cmd="$(cut -d" " -f1  <<< "${line}")"
         local args="$(cut -d" " -f2- <<< "${line}")"
