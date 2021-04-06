a9894bdb87fda5d0157ea8885864a33b3041d803
Zamarin Arthur <arthurzam@gmail.com>
2021-04-06 08:58:33 +0300

    periodic-backup.sh: improve script
    add printing backup size - enables to check that upload is complete

--- scripts/periodic-backup.sh
+++ scripts/periodic-backup.sh
@@ -4,9 +4,12 @@
 
 MOUNTPOINT="/media/box"
 BACKUP_RELPATH="Backup/periodic"
+PASS_FILE="/usr/local/bin/periodic-backup-PASSFILE"
+
+[[ ! -e "${PASS_FILE}" ]] && echo "Error: missing password file at ${PASS_FILE}" >&2 && exit 1
 
 if ! grep -qs "${MOUNTPOINT}" /proc/mounts; then
-    export _unmount_box="1"
+    _unmount_box="1"
     systemctl start rclone@box.service
 fi
 
@@ -17,16 +20,14 @@ _backup() {
     # $2,3,... - 
     local _name="${1}"
     shift
-    gpg --output "${_name}-$(date +%Y%m%d).tar.xz.enc" --batch --passphrase-file="/usr/local/bin/periodic-backup-PASSFILE" --symmetric --cipher-algo AES256 <(tar cJ -C / "$@") && echo "Backuped ${_name}"
+    local DST_FILENAME="${MOUNTPOINT}/${BACKUP_RELPATH}/${_name}-$(date +%Y%m%d).tar.xz.enc"
+    [[ -e "${DST_FILENAME}" ]] && rm "${DST_FILENAME}"
+    gpg --output "${DST_FILENAME}" --batch --passphrase-file="${PASS_FILE}" --symmetric --cipher-algo AES256 <(tar cJ -C / "$@") && echo "Backuped ${_name}: $(du -h "${DST_FILENAME}" | awk '{print $1}')"
 }
-
-pushd "${MOUNTPOINT}/${BACKUP_RELPATH}" >& /dev/null
 
 _backup QMPlay2 home/arthur/.qmplay2/
 _backup sismaot home/arthur/{sismaot.kdbx,Backup/sismaot.key}
 _backup firefox home/arthur/.mozilla/firefox/
-
-popd >& /dev/null
 
 if [[ -n ${_unmount_box} ]]; then
     systemctl stop rclone@box.service
